da03f6cbbe66b0219326c8711b7fdf3b7a5aa7b9
Alex McGuinness <github@alexmcguinness.me.uk>
2024-01-21 00:00:07 +0000

    convert to expressjs style and add reset functionality

--- app.js
+++ app.js
@@ -1,48 +1,207 @@
 const http = require('http');
 var fs = require('fs');
+const express = require('express')
+const app = express()
+
+var idRouter = express.Router({mergeParams: true}); 
+//mergeParams allows the next router to access params
+//var actionRouter = express.Router({mergeParams: true}); 
+
+/**
+/db5/lights/on
+/db5/lights/off
+/db5/status/lights
+/db5/status/vcc
+/db5/admin/reset
+*/
 
 //const hostname = '127.0.0.1';
 const hostname = '192.168.0.124';
 const port = 3000;
 
+lights_tag = "lights";
+reset_tag = "reset";
 
-led_state = {
+var data = {}
+
+
+//lights_state = "off"
+
+state = {
    lights: "off",
-   reset: "false"
+   reset: 0
 }
 
-const server = http.createServer((req, res) => {
+//idRouter.use('/:id', actionRouter);
+app.use('/:id', idRouter);
+
+idRouter.route('/')
+   .get(function (req, res) {
+      console.log("idRouter /")
+//      res.json({
+//         lights: 'on',
+//         reset: 'false'
+//      }) 
+      res.status(200)
+         .send("No Action");
+   });
+
+idRouter.route('/status')
+   .get(function (req, res){
+      //res.status(200).send('hello id ' + req.params.id);
+      rc = {}
+      if (req.params.id in data){
+         node = data[req.params.id] 
+         console.log(node)
+         rc = node
+      } else {
+         data[req.params.id] = {}
+      }
+      res.json(rc)
+
+      //Reset to false after called once
+      data[req.params.id]['reset'] = "false"; 
+  });
+
+idRouter.route('/status/:metric')
+   .get(function (req, res){
+//      res.status(200).send('status id ' + req.params.id + " metric:" + req.params.metric);
+      console.log('status id ' + req.params.id + " metric:" + req.params.metric);
+      rc = {}
+      if (req.params.id in data){
+         node = data[req.params.id] 
+         if (req.params.metric in node){
+            node[req.params.metric] 
+            console.log(node)
+            rc = { [req.params.metric] : node[req.params.metric] }
+         }
+      }
+      res.json(rc)
+  });
+
+idRouter.route('/lights/:state')
+   .put(function (req, res){
+      if (!(req.params.id in data)){
+         data[req.params.id] = {}
+      }
+      node = data[req.params.id]
+      node["lights"] = req.params.state
+
+      //res.status(200).send("set lights for " + req.params.id + " " + req.params.state);
+      res.json(data)
+      console.log('status id ' + req.params.id + " lights:" + req.params.state);
+  });
+
+idRouter.route('/admin/reset')
+   .put(function (req, res){
+      if (!(req.params.id in data)){
+         data[req.params.id] = {}
+      }
+      node = data[req.params.id]
+      node["reset"] = "true"
+
+      //res.status(200).send("set lights for " + req.params.id );
+      res.json(data)
+      console.log('reset id ' + req.params.id);
+  });
+
+//actionRouter.route('/')
+//   .get(function (req, res) {
+//      res.status(200)
+//         .send('hello actions from id '  +req.params.id);
+//  })
+
+//app.get('/:id/status', (req, res) => {
+//   id = +req.params["id"]
+//   console.log("getting status")
+//   console.log("ID:"+id)
+//   console.log("STATE:"+JSON.stringify(state))
+//   //res.send(JSON.stringify(state))
+//   res.send(req.params)
+//})
+//
+//app.get('/:id/status/:element', (req, res) => {
+//   id = +req.params["id"]
+//   el = +req.params["element"]
+//   console.log("getting status")
+//   console.log("ID:"+id)
+//   console.log("Element:"+element)
+//   console.log("STATE:"+JSON.stringify(state))
+//
+//   if (!(id in data)){
+//      data[id] = {element : 0}
+//   }
+
+//   //res.send(JSON.stringify(state))
+//   res.send(req.params)
+//})
+
+//app.put('/:id/lights/:state', (req, res) => {
+//   console.log("setting lights")
+//   id = req.params["id"]
+//   state = req.params["state"]
+//   console.log("ID:"+id)
+//   console.log("STATE:"+state)
+//
+//   if (!(id in data)){
+//      data[id] = {}
+//   }
+//   node = data[id]
+//   node["lights"] = state
+//   
+//   console.log("STATE:"+JSON.stringify(data))
+//
+//   res.send(JSON.stringify(data))
+//})
 
 
-  res.setHeader('Content-Type', 'text/plain');
-  if( req.method === 'GET') {
-     if (req.url === '/') {
-        res.end('<h1>Hello World</h1>');
-     }else if (req.url === '/admin/vcc') {
-        res.end('<h1>VCC Status</h1>');
-     }else if (req.url === '/db5/light/status') {
-        console.log("REQ:"+JSON.stringify(led_state))
-        res.end(JSON.stringify(led_state));
-     } else{
-        res.end("Doh");
-     }
-  
-  } else if( req.method === 'PUT') {
-     if (req.url === '/db5/light/off') {
-        led_state["lights"] = "off";
-     }else if (req.url === '/db5/light/on') {
-        led_state["lights"] = "on";
-     } else {
-        res.end(`{"error": "${http.STATUS_CODES[404]}"}`)
-     }
-     console.log(JSON.stringify(led_state))
-     //res.end("<h1>Light Status</h1>\n<lights>"+led_state+"</lights>");
-     res.end(JSON.stringify(led_state));
-  }
+//led_state = {
+//   lights: state["lights"]
+//}
 
-//  res.statusCode = 200;
-});
+//const server = http.createServer((req, res) => {
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+
+//  res.setHeader('Content-Type', 'text/plain');
+//  if( req.method === 'GET') {
+//     if (req.url === '/') {
+//        res.end(`{"error": "${http.STATUS_CODES[404]}"}`)
+//     }else if (req.url === '/db5/status') {
+//        console.log("REQ:"+JSON.stringify(state))
+//        res.end(JSON.stringify(state));
+//        state[reset] = 0
+//     }else if (req.url === '/db5/status/vcc') {
+//        res.end('<h1>VCC Status</h1>');
+//     }else if (req.url === '/db5/status/lights') {
+//        console.log("REQ:"+JSON.stringify(led_state))
+//        res.end(JSON.stringify(led_state));
+//     } else{
+//        res.end(`{"error": "${http.STATUS_CODES[404]}"}`)
+//     }
+//  
+//  } else if( req.method === 'PUT') {
+//     if (req.url === '/db5/lights/off') {
+//        state[lights_tag] = "off";
+//        //lights_state = "off"
+//     }else if (req.url === '/db5/lights/on') {
+//        state[lights_tag] = "on";
+//        //lights_state = "on"
+//     }else if (req.url === '/db5/admin/reset') {
+//        //led_state[reset_tag] = "on";
+//        state[reset] = 0
+//     } else {
+//        res.end(`{"error": "${http.STATUS_CODES[404]}"}`)
+//     }
+//     console.log(JSON.stringify(state))
+//
+//     res.end(JSON.stringify(state));
+//  }
+//});
+
+//server.listen(port, hostname, () => {
+//  console.log(`Server running at http://${hostname}:${port}/`);
+//});
+
+app.listen(port, hostname, () => {
+   console.log(`Server running at http://${hostname}:${port}/`);
+})
--- set_light_rest_server.py
+++ set_light_rest_server.py
@@ -3,56 +3,83 @@
 import getopt, sys
 import requests
 
+
+
+#/db5/lights/on
+#/db5/lights/off
+#/db5/status/lights
+#/db5/status/vcc
+#/db5/admin/reset
+
 def usage():
-   print(sys.argv[0] + " [-v] [-l light_state] [-h host]")
+   print(sys.argv[0] + "  [-l light_state] [-h host] [--reset] [-s lights|vcc]")
+
+end_point = None
+host = "192.168.0.124"
+port = 3000
+id = ""
+command = None
+
 
 try:
-   opts, args = getopt.getopt(sys.argv[1:], "?l:vh:", ["help", "lights=", "host="])
+   opts, args = getopt.getopt(sys.argv[1:], "?l:h:p:s:i:", ["help", "lights=", "host=", "port=", "reset", "status=", "id="])
+
+   for o, a in opts:
+      if o in ("-l", "--lights="):
+         end_point = "/lights/" + a
+         command = "PUT"
+      elif o in ("-s", "--status="):
+         if "all" == a:
+            end_point = "/status"
+         else:
+            end_point = "/status/" + a
+         command = "GET"
+      elif o in ("-h", "--host="):
+         host = a
+      elif o in ("-p", "--port="):
+         port = a
+      elif o in ("-i", "--id="):
+         id = a
+      elif o in ("--reset"):
+         command = "PUT"
+         end_point = "/admin/reset"
+      else:
+         print("o=" + str(o) + " a=" + str(a))
+         assert False, "unhandled option"
+
 except getopt.GetoptError as err:
    # print help information and exit:
    print(err)  # will print something like "option -a not recognized"
    usage()
 #   sys.exit(2)
 
-status = None
-command = None
-host = "192.168.0.124"
-port = 3000
-for o, a in opts:
-   if o in ("-l", "--lights="):
-      command = "db5/light"
-      status = a
-   elif o in ("-v", "--vcc="):
-      command = "admin"
-      status = "vcc"
-   elif o in ("-h", "--host="):
-      host = a
-   else:
-      print("o=" + str(o) + " a=" + str(a))
-      assert False, "unhandled option"
+#reset = False
+#status = None
 
-if not command or not status:
+if not end_point or not id:
    usage()
    sys.exit()
 
 
-
 import http.client
-
 
 headers = {}
 
-end_point = "/"+command+"/" + status
-url = "http://" + host +":"+ str(port) + end_point
-
-print("URL:" + url)
-
 try:
 
-   if status in ["on", "off"]:
-      r = requests.put(url )
-   else:
+#   url = "http://" + host +":"+ str(port)
+#   if reset: 
+#      end_point = "/admin/reset" 
+#   else:
+#      end_point = "/"+command+"/" + status
+
+   url = "http://" + host +":"+ str(port) + "/" + id + end_point
+   print(command + " URL:" + url)
+
+   if command == "GET":
       r = requests.get(url )
+   elif command == "PUT":
+      r = requests.put(url )
 
    data = r.json()
    print(data)
